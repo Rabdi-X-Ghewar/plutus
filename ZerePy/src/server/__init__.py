a29e329abeba0ace1b41a801722547ab086baeca
Goutam-11 <159244984+Goutam-11@users.noreply.github.com>
2025-03-10 18:59:31 +0530

    Update __init__.py

--- ZerePy/src/server/__init__.py
+++ ZerePy/src/server/__init__.py
@@ -1,7 +1,11 @@
 import uvicorn
-from .app import create_app
 
 def start_server(host: str = "0.0.0.0", port: int = 8000):
     """Start the ZerePy server"""
-    app = create_app()
-    uvicorn.run(app, host=host, port=port,reload=True)
+    uvicorn.run(
+        "src.server.app:create_app",
+        host=host,
+        port=port,
+        reload=True,
+        factory=True
+    )
